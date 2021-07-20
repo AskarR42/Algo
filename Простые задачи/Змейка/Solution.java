88c401100de8fd50987f9de324af2215d5602c43
AskarR42 <59599709+AskarR42@users.noreply.github.com>
2021-07-20 17:57:21 +0300

    Create Solution.java

---
+++ Простые задачи/Змейка/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int counter = 1;
+        for (int boarder = 0; boarder < (n + 1) / 2; boarder++) {
+            for (int j = boarder; j < n - boarder; j++) {
+                matrix[boarder][j] = counter++;
+            }
+            for (int i = boarder + 1; i < n - boarder; i++) {
+                matrix[i][n - 1 - boarder] = counter++;
+            }
+            for (int j = n - 2 - boarder; j >= boarder; j--) {
+                matrix[n - 1 - boarder][j] = counter++;
+            }
+            for (int i = n - 2 - boarder; i >= boarder + 1; i--) {
+                matrix[i][boarder] = counter++;
+            }
+        }
+        return matrix;
+    }
+}
