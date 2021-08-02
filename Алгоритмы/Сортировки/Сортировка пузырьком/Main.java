920127987d247c3f06515b365ee26dee10460907
AskarR42 <59599709+AskarR42@users.noreply.github.com>
2021-08-02 20:07:00 +0300

    Create Main.java

---
+++ Алгоритмы/Сортировки/Сортировка пузырьком/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    private static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
