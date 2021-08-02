457bc8c8f27a183ff1ae1d74eae6931e6dcf2b29
AskarR42 <59599709+AskarR42@users.noreply.github.com>
2021-08-02 20:17:43 +0300

    Create Main.java

---
+++ Алгоритмы/Сортировки/Сортировка шейкером/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    private static void cocktailSort(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+
+        int left = 0;
+        int right = arr.length - 1;
+        while (left != right) {
+            for (int i = left; i < right; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+            left++;
+            for (int i = right; i > left; i--) {
+                if (arr[i] < arr[i - 1]) {
+                    swap(arr, i, i - 1);
+                }
+            }
+            right--;
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+}
