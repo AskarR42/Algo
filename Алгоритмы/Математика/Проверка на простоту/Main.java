aaf4b2de798af5bdba28be16747e561cf46376ce
AskarR42 <59599709+AskarR42@users.noreply.github.com>
2021-10-04 13:16:52 +0300

    Create Main.java

---
+++ Алгоритмы/Математика/Проверка на простоту/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    private static boolean prime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int x = 2; x * x < n; x++) {
+            if (n % x == 0) {
+                return  false;
+            }
+        }
+
+        return true;
+    }
+}
